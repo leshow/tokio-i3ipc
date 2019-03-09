a58f64f04c389ba691d9e36d2fd4cfae79e50dbb
Evan Cameron <cameron.evan@gmail.com>
2019-03-09 02:15:18 -0500

    Add events too

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate serde_derive;
 
+pub mod event;
 pub mod reply;
 
 #[cfg(test)]
--- src/reply.rs
+++ src/reply.rs
@@ -1,26 +1,53 @@
 use std::collections::HashMap;
-// TODO manually impl Eq
+
+#[derive(Deserialize, Serialize, Eq, PartialEq, Clone, Hash, Debug)]
+pub struct Success {
+    pub success: bool,
+}
+
+#[derive(Deserialize, Serialize, Eq, PartialEq, Clone, Hash, Debug)]
+pub struct Workspace {
+    pub num: i32,
+    pub name: String,
+    pub visible: bool,
+    pub focused: bool,
+    pub urgent: bool,
+    pub rect: Rect,
+    pub output: String,
+}
+
+#[derive(Deserialize, Serialize, Eq, PartialEq, Clone, Hash, Debug)]
+pub struct Outputs {
+    pub name: String,
+    pub active: bool,
+    pub primary: bool,
+    pub current_workspace: Option<String>,
+    pub rect: Rect,
+}
+
+/// Node reply
 #[derive(Deserialize, Serialize, PartialEq, Clone, Debug)]
-struct Node {
-    id: i32,
-    name: Option<String>,
+// TODO manually impl Eq
+pub struct Node {
+    pub id: i32,
+    pub name: Option<String>,
     #[serde(rename = "type")]
-    node_type: NodeType,
-    output: Option<String>,
-    orientation: NodeOrientation,
-    border: NodeBorder,
-    percent: Option<f64>,
-    rect: Rect,
-    window_rect: Rect,
-    deco_rect: Rect,
-    geometry: Rect,
-    window_properties: Option<HashMap<WindowProperty, Option<String>>>,
-    urgent: bool,
-    focused: bool,
-    focus: Vec<i64>,
-    sticky: bool,
-    floating_nodes: Vec<Node>,
-    nodes: Vec<Node>,
+    pub node_type: NodeType,
+    pub output: Option<String>,
+    pub orientation: NodeOrientation,
+    pub border: NodeBorder,
+    pub percent: Option<f64>,
+    pub rect: Rect,
+    pub window_rect: Rect,
+    pub deco_rect: Rect,
+    pub geometry: Rect,
+    pub window_properties: Option<HashMap<WindowProperty, Option<String>>>,
+    pub urgent: bool,
+    pub focused: bool,
+    pub focus: Vec<i64>,
+    pub sticky: bool,
+    pub floating_nodes: Vec<Node>,
+    pub nodes: Vec<Node>,
 }
 
 #[derive(Deserialize, Serialize, Eq, PartialEq, Clone, Hash, Debug)]
@@ -35,15 +62,15 @@ pub enum WindowProperty {
 
 #[derive(Deserialize, Serialize, Eq, PartialEq, Clone, Hash, Debug)]
 pub struct Rect {
-    x: i32,
-    y: i32,
-    width: i32,
-    height: i32,
+    pub x: i32,
+    pub y: i32,
+    pub width: i32,
+    pub height: i32,
 }
 
 #[derive(Deserialize, Serialize, Eq, PartialEq, Clone, Hash, Debug)]
 #[serde(rename_all = "snake_case")]
-enum NodeType {
+pub enum NodeType {
     Root,
     Output,
     Con,
@@ -75,4 +102,72 @@ pub enum NodeOrientation {
     Horizontal,
     Vertical,
     None,
+}
+
+/// Marks Reply
+#[derive(Deserialize, Serialize, Eq, PartialEq, Hash, Debug, Clone)]
+pub struct Marks(Vec<String>);
+
+/// BarIds
+#[derive(Deserialize, Serialize, Eq, PartialEq, Hash, Debug, Clone)]
+pub struct BarIds(Vec<String>);
+
+/// BarConfig Reply
+#[derive(Deserialize, Serialize, Eq, PartialEq, Debug, Clone)]
+pub struct BarConfig {
+    pub id: String,
+    pub mode: String,
+    pub position: String,
+    pub status_command: String,
+    pub font: String,
+    pub workspace_buttons: bool,
+    pub binding_mode_indicator: bool,
+    pub verbose: bool,
+    pub colors: HashMap<BarPart, String>,
+}
+
+#[derive(Deserialize, Serialize, Eq, PartialEq, Hash, Debug, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum BarPart {
+    Background,
+    Statusline,
+    Separator,
+    FocusedBackground,
+    FocusedStatusline,
+    FocusedSeparator,
+    FocusedWorkspaceText,
+    FocusedWorkspaceBg,
+    FocusedWorkspaceBorder,
+    ActiveWorkspaceText,
+    ActiveWorkspaceBg,
+    ActiveWorkspaceBorder,
+    InactiveWorkspaceText,
+    InactiveWorkspaceBg,
+    InactiveWorkspaceBorder,
+    UrgentWorkspaceText,
+    UrgentWorkspaceBg,
+    UrgentWorkspaceBorder,
+    BindingModeText,
+    BindingModeBg,
+    BindingModeBorder,
+}
+
+/// Version reply
+#[derive(Deserialize, Serialize, Eq, PartialEq, Hash, Debug, Clone)]
+pub struct Version {
+    pub major: i32,
+    pub minor: i32,
+    pub patch: i32,
+    pub human_readable: String,
+    pub loaded_config_file_name: String,
+}
+
+/// Binding Modes Reply
+#[derive(Deserialize, Serialize, Eq, PartialEq, Hash, Debug, Clone)]
+pub struct BindingModes(Vec<String>);
+
+/// Config Reply
+#[derive(Deserialize, Serialize, Eq, PartialEq, Hash, Debug, Clone)]
+pub struct Config {
+    pub config: String,
 }

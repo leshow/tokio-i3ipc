c515cbebd35198b45682024ccda8a1dd8b2f31db
Evan Cameron <cameron.evan@gmail.com>
2019-03-09 15:53:41 -0500

    Start work on tokio part

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
+use serde::de::DeserializeOwned;
+use tokio_uds::UnixStream;
+
+use std::io;
+
 pub mod event;
 pub mod msg;
 pub mod reply;
+
+#[derive(Debug)]
+pub struct I3IPC {
+    stream: UnixStream,
+}
+
+pub struct MsgResponse<D> {
+    msg_type: msg::Msg,
+    payload: D,
+}
+
+trait I3 {
+    fn conn(&self) -> io::Result<String>;
+    fn send_msg<P>(&mut self, msg: msg::Msg, payload: P) -> io::Result<()>
+    where
+        P: AsRef<str>;
+    fn receive_msg<D: DeserializeOwned>(&mut self) -> io::Result<MsgResponse<D>>;
+}
 
 #[cfg(test)]
 mod tests {
--- src/msg.rs
+++ src/msg.rs
@@ -1,5 +1,5 @@
 #[derive(Eq, PartialEq, Hash, Debug, Clone)]
-pub enum Type {
+pub enum Msg {
     RunCommand,
     Workspaces,
     Subscribe,
@@ -14,21 +14,21 @@ pub enum Type {
     Sync,
 }
 
-impl From<usize> for Type {
+impl From<usize> for Msg {
     fn from(num: usize) -> Self {
         match num {
-            0 => Type::RunCommand,
-            1 => Type::Workspaces,
-            2 => Type::Subscribe,
-            3 => Type::Outputs,
-            4 => Type::Tree,
-            5 => Type::Marks,
-            6 => Type::BarConfig,
-            7 => Type::Version,
-            8 => Type::BindingModes,
-            9 => Type::Config,
-            10 => Type::Tick,
-            11 => Type::Sync,
+            0 => Msg::RunCommand,
+            1 => Msg::Workspaces,
+            2 => Msg::Subscribe,
+            3 => Msg::Outputs,
+            4 => Msg::Tree,
+            5 => Msg::Marks,
+            6 => Msg::BarConfig,
+            7 => Msg::Version,
+            8 => Msg::BindingModes,
+            9 => Msg::Config,
+            10 => Msg::Tick,
+            11 => Msg::Sync,
             _ => panic!("Unknown message type found"),
         }
     }

b24de7ea2acd03e77492a880215b8a44caef1a48
Evan Cameron <cameron.evan@gmail.com>
2019-03-20 23:26:03 -0400

    getting responses

--- src/codec.rs
+++ src/codec.rs
@@ -3,6 +3,11 @@ use futures::prelude::*;
 use tokio::prelude::*;
 use tokio_uds::UnixStream;
 
+use super::event::Event;
+use super::msg::Msg;
+use super::reply;
+use super::{I3Connect, I3Stream};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use std::{
     env,
     io::{self, Read, Write},
@@ -11,10 +16,6 @@ use std::{
     process::Command,
 };
 
-use super::event::Event;
-use super::msg::Msg;
-use super::{I3Connect, I3Stream};
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -22,25 +23,41 @@ mod test {
     #[test]
     fn read_socket() -> io::Result<()> {
         let fut = UnixStream::connect(I3Connect::socket_path()?)
-            .and_then(|s| {
+            .and_then(|stream| {
                 // let (tx, rx) = s.split();
 
-                let sub_json = "[ \"window\" ]";
-                let mut buf = BytesMut::with_capacity(14 + sub_json.len());
-                buf.put_slice(b"i3-ipc");
-                buf.put_u32_le(sub_json.len() as u32);
-                buf.put_u32_le(2);
-                buf.put_slice(sub_json.as_bytes());
-                println!("{:?}", buf);
+                let events = [Event::Window];
+                let payload = serde_json::to_string(&events).unwrap();
+                // let mut buf = BytesMut::with_capacity(14 + payload.len());
+                // buf.put_slice(I3Stream::MAGIC.as_bytes());
+                // buf.put_u32_le(payload.len() as u32);
+                // buf.put_u32_le(2);
+                // buf.put_slice(payload.as_bytes());
+                // println!("{:#?}", buf);
+                let mut buf = Vec::with_capacity(14 + payload.len());
+                buf.extend("i3-ipc".bytes()); // 6 bytes
+                buf.write_u32::<LittleEndian>(payload.len() as u32).unwrap(); // 4 bytes
+                buf.write_u32::<LittleEndian>(2).unwrap(); // 4 bytes
+                buf.extend(payload.bytes()); // payload.len() bytes
 
-                tokio::io::write_all(s, buf)
+                println!("{:#?}", buf);
+                tokio::io::write_all(stream, buf)
             })
             .and_then(|(stream, _buf)| {
-                let mut buf: Vec<u8> = vec![0; 256];
+                let mut buf: Vec<u8> = vec![0; 30];
                 tokio::io::read_exact(stream, buf)
             })
             .inspect(|(_stream, buf)| {
                 println!("buf: {:?}", buf);
+            })
+            .map(|(_stream, buf)| {
+                let s = String::from_utf8(buf).unwrap();
+                println!("{:?}", s);
+                let out: reply::Node = serde_json::from_slice(s.as_bytes()).unwrap();
+                out
+            })
+            .inspect(|node| {
+                println!("node: {:?}", node);
             })
             .map(|_| ())
             .map_err(|e| eprintln!("{:?}", e));

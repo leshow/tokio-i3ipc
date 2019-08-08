48a7d0293442862c05fe5bccc1658bdd7f86b2ec
Evan Cameron <evan@vectorface.com>
2019-08-08 16:55:07 -0400

    Move to more synchronous API

--- src/lib.rs
+++ src/lib.rs
@@ -68,38 +68,15 @@
 
 pub use i3ipc_types::*;
 pub mod codec;
-pub mod get;
-pub mod io;
 mod util;
 
 pub use util::*;
+use codec::EventCodec;
 
-use futures::{ready, Future};
-use std::{io as stio, pin::Pin, task::{Context, Poll}};
-use tokio::io::{AsyncRead, AsyncWrite, AsyncReadExt, AsyncWriteExt};
-use tokio_uds::{ConnectFuture, UnixStream};
-
-/// Newtype wrapper for `ConnectFuture` meant to resolve some Stream, mostly likely `UnixStream`
-#[derive(Debug)]
-pub struct I3 {
-    conn: ConnectFuture,
-}
-
-// /// Provides a `connect` function for types which returns a `Future`
-pub trait Connect {
-    type Connected: AsyncI3IPC;
-    type Future: Future<Output=stio::Result<Self::Connected>>;
-
-    fn connect() -> stio::Result<Self::Future>;
-}
-
-impl Connect for I3 {
-    type Connected = UnixStream;
-    type Future = ConnectFuture;
-    fn connect() -> stio::Result<Self::Future> {
-        Ok(UnixStream::connect(socket_path()?))
-    }
-}
+use serde::de::DeserializeOwned;
+use std::io;
+use tokio::{codec::FramedRead, io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt}};
+use tokio_uds::UnixStream;
 
 /// [I3IPC](trait.I3IPC.html) provides default implementations for reading/writing buffers into a format i3 understands. This
 /// trait expresses that + asynchronousity
@@ -110,17 +87,175 @@ impl AsyncI3IPC for UnixStream {}
 impl<'a, T: ?Sized + AsyncI3IPC + Unpin> AsyncI3IPC for &'a mut T {}
 impl<T: ?Sized + AsyncI3IPC + Unpin> AsyncI3IPC for Box<T> {}
 
-// Implement `Future` for [I3](struct.I3.html) so it can be polled into a ready `UnixStream`
-impl Future for I3 where {
-    type Output = stio::Result<UnixStream>;
-    fn poll(mut self: Pin<&mut Self>, ctx: &mut Context<'_>) -> Poll<Self::Output> {
-        let stream = ready!(self.conn.poll(ctx));
-        Poll::Ready(Ok(stream))
-    }
+/// Newtype wrapper for `ConnectFuture` meant to resolve some Stream, mostly likely `UnixStream`
+#[derive(Debug)]
+pub struct I3 {
+    stream: UnixStream,
 }
 
-// impl I3 {
-//     pub async fn connect(&mut self) -> stio::Result<UnixStream> {
-//         Ok(self.conn.await?)
-//     }
-// }
+// Implement `Future` for [I3](struct.I3.html) so it can be polled into a ready `UnixStream`
+impl I3 {
+    /// Sends a message and payload, used for `get_*` commands and `run_command`
+    async fn _send_msg<P>(&mut self, msg: msg::Msg, payload: Option<P>) -> io::Result<()>
+    where
+        P: AsRef<str>,
+    {
+        let buf = self.stream._encode_msg(msg, payload);
+        self.stream.write_all(&buf).await
+    }
+    async fn _decode_msg(&mut self) -> io::Result<(u32, Vec<u8>)> {
+        let mut init = [0_u8; 14];
+        let _len = self.stream.read_exact(&mut init).await?;
+
+        if &init[0..6] != MAGIC.as_bytes() {
+            panic!("Magic str not received");
+        }
+        let payload_len = u32::from_ne_bytes([init[6], init[7], init[8], init[9]]) as usize;
+        let msg_type = u32::from_ne_bytes([init[10], init[11], init[12], init[13]]);
+
+        let mut payload = vec![0_u8; payload_len];
+        let _len_read = self.stream.read_exact(&mut payload).await?;
+
+        Ok((msg_type, payload))
+    }
+
+    /// Connects to I3 over `UnixStream`
+    pub async fn connect() -> io::Result<Self> {
+        Ok(I3 {
+            stream: UnixStream::connect(socket_path()?).await?,
+        })
+    }
+
+    pub async fn send_msg_body<P>(&mut self, msg: msg::Msg, payload: P) -> io::Result<()>
+    where
+        P: AsRef<str>,
+    {
+        self._send_msg(msg, Some(payload)).await
+    }
+
+    pub async fn send_msg(&mut self, msg: msg::Msg) -> io::Result<()> {
+        self._send_msg::<&str>(msg, None).await
+    }
+
+    /// Receive some message from the socket. Holds a `Msg` type and payload
+    pub async fn read_msg<D>(&mut self) -> io::Result<MsgResponse<D>>
+    where
+        D: DeserializeOwned,
+    {
+        let (msg_type, payload) = self._decode_msg().await?;
+        Ok(MsgResponse {
+            msg_type: msg_type.into(),
+            body: serde_json::from_slice(&payload[..])?,
+        })
+    }
+
+    /// Like `read_msg` but for `event::Event`
+    pub async fn read_event(&mut self) -> io::Result<event::Event> {
+        let (evt_type, payload_bytes) = self._decode_msg().await?;
+        decode_event(evt_type, payload_bytes)
+    }
+
+    /// Send a `Msg` and payload and receive a response. Convenience function over `send_msg` and `read_msg`
+    pub async fn send_read<P, D>(&mut self, msg: msg::Msg, payload: P) -> io::Result<MsgResponse<D>>
+    where
+        P: AsRef<str>,
+        D: DeserializeOwned,
+    {
+        self.send_msg_body(msg, payload).await?;
+        self.read_msg().await
+    }
+
+    /// Returns a Future that will send a [Subscribe](event/enum.Subscribe.html) message to i3 along with a list of events to listen to.
+    pub async fn subscribe<E>(&mut self, events: E) -> io::Result<MsgResponse<reply::Success>>
+    where
+        E: AsRef<[event::Subscribe]>,
+    {
+        let sub_json = serde_json::to_string(events.as_ref())?;
+        self.send_msg_body(msg::Msg::Subscribe, sub_json).await?;
+        Ok(self.read_msg::<reply::Success>().await?)
+    }
+
+    /// Provides a type that implements `Stream` so you can `await` events in a loop
+    pub fn listen(self) -> FramedRead<UnixStream, D> {
+         FramedRead::new(self.stream, EventCodec)
+    }
+
+    /// Run an arbitrary command on i3. Response is a `Vec` of success true/false.
+    pub async fn run_command<S: AsRef<str>>(
+        &mut self,
+        payload: S,
+    ) -> io::Result<Vec<reply::Success>> {
+        self.send_msg_body(msg::Msg::RunCommand, payload).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Future for getting the current [Workspaces](../reply/struct.Workspace.html), sends [Workspaces](../msg/enum.Msg.html#variant.Workspaces)
+    pub async fn get_workspaces(&mut self) -> io::Result<reply::Workspaces> {
+        self.send_msg(msg::Msg::Workspaces).await?;
+        let resp: MsgResponse<Vec<reply::Workspace>> = self.read_msg().await?;
+        Ok(resp.body)
+    }
+
+    /// Future that gets all [Outputs](../reply/struct.Outputs.html), sends [Outputs](../msg/enum.Msg.html#variant.Outputs)
+    pub async fn get_outputs(&mut self) -> io::Result<reply::Outputs> {
+        self.send_msg(msg::Msg::Outputs).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Future to get complete [Node](../reply/struct.Node.html), sends [Tree](../msg/enum.Msg.html#variant.Tree)
+    pub async fn get_tree(&mut self) -> io::Result<reply::Node> {
+        self.send_msg(msg::Msg::Tree).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Get all [Marks](../reply/struct.Marks.html), sends [Marks](../msg/enum.Msg.html#variant.Marks)
+    pub async fn get_marks(&mut self) -> io::Result<reply::Marks> {
+        self.send_msg(msg::Msg::Marks).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Future to get all [BarIds](../reply/struct.BarIds.html), sends [BarConfig](../msg/enum.Msg.html#variant.BarConfig)
+    pub async fn get_bar_ids(&mut self) -> io::Result<reply::BarIds> {
+        self.send_msg(msg::Msg::BarConfig).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Future to get configs associated with a bar id responds with [BarConfig](../reply/struct.BarConfig.html), sends [BarConfig](../msg/enum.Msg.html#variant.BarConfig)
+    pub async fn get_bar_config<S: AsRef<str>>(
+        &mut self,
+        bar_id: S,
+    ) -> io::Result<reply::BarConfig> {
+        self.send_msg_body(msg::Msg::BarConfig, bar_id).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Get i3 version
+    pub async fn get_version(&mut self) -> io::Result<reply::Version> {
+        self.send_msg(msg::Msg::Version).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Future to get [BindingModes](../reply/struct.BindingModes.html), sends [BindingModes](../msg/enum.Msg.html#variant.BindingModes)
+    pub async fn get_binding_modes(&mut self) -> io::Result<reply::BindingModes> {
+        self.send_msg(msg::Msg::BindingModes).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Future for [Config](../reply/struct.Config.html), sends [Config](../msg/enum.Msg.html#variant.Config)
+    pub async fn get_config(&mut self) -> io::Result<reply::Config> {
+        self.send_msg(msg::Msg::Config).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Future sends [Tick](../msg/enum.Msg.html#variant.Tick)
+    pub async fn get_tick(&mut self) -> io::Result<reply::Success> {
+        self.send_msg(msg::Msg::Tick).await?;
+        Ok(self.read_msg().await?.body)
+    }
+
+    /// Future [Sync](../msg/enum.Msg.html#variant.Sync)
+    pub async fn get_sync(&mut self) -> io::Result<reply::Success> {
+        self.send_msg(msg::Msg::Sync).await?;
+        Ok(self.read_msg().await?.body)
+    }
+}
--- src/util.rs
+++ src/util.rs
@@ -1,66 +1,47 @@
-use crate::{io as i3io, *};
+use crate::*;
 
 use futures::prelude::*;
 use futures::{channel::mpsc::Sender, Future};
 use serde::de::DeserializeOwned;
 use std::io as stio;
 use tokio::codec::FramedRead;
-use tokio::io::{AsyncRead};
+use tokio::io::AsyncRead;
 
 /// Convenience function that decodes a single response and passes the type and undecoded buffer to a closure
-pub async fn decode_response<F, T, S>(stream: S, f: F) -> stio::Result<(S, T)>
+pub async fn decode_response<F, T, S>(stream: &mut S, f: F) -> stio::Result<T>
 where
     F: Fn(u32, Vec<u8>) -> T,
     S: AsyncRead + Unpin,
 {
-    let buf = [0; 14];
+    let mut buf = [0; 14];
     stream.read_exact(&mut buf).await?;
- 
     if &buf[0..6] != MAGIC.as_bytes() {
         panic!("Magic str not received");
     }
     let payload_len = u32::from_ne_bytes([buf[6], buf[7], buf[8], buf[9]]) as usize;
     let msg_type = u32::from_ne_bytes([buf[10], buf[11], buf[12], buf[13]]);
 
-    let payload = vec![0; payload_len];
+    let mut payload = vec![0; payload_len];
     stream.read_exact(&mut payload).await?;
-    Ok((stream, f(msg_type, payload)))
+    Ok(f(msg_type, payload))
 }
 
 /// Decode a response into a [MsgResponse](struct.MsgResponse.html)
-pub fn decode_msg<D, S>(
-    stream: S,
-) -> impl Future<Output = stio::Result<(S, stio::Result<MsgResponse<D>>)>>
+pub async fn decode_msg<D, S>(stream: &mut S) -> stio::Result<stio::Result<MsgResponse<D>>>
 where
     D: DeserializeOwned,
     S: AsyncRead + Unpin,
 {
-    decode_response(stream, MsgResponse::new)
+    decode_response(stream, MsgResponse::new).await
 }
 
 /// Decode a response into an [Event](event/enum.Event.html)
-pub fn decode_event_future<D, S>(
-    stream: S,
-) -> impl Future<Output = stio::Result<(S, stio::Result<event::Event>)>>
+pub async fn decode_event_future<D, S>(stream: &mut S) -> stio::Result<stio::Result<event::Event>>
 where
     D: DeserializeOwned,
     S: AsyncRead + Unpin,
 {
-    decode_response(stream, decode_event)
-}
-
-/// Function returns a Future that will send a [Subscribe](event/enum.Subscribe.html) to i3 along with the events to listen to.
-/// Is bounded by [AsyncI3IPC](trait.AsyncI3IPC.html) but you should almost always use `UnixStream`
-pub async fn subscribe_future<S, E>(
-    stream: S,
-    events: E,
-) -> stio::Result<(S, MsgResponse<reply::Success>)>
-where
-    S: AsyncI3IPC + Unpin,
-    E: AsRef<[event::Subscribe]>,
-{
-    let s = i3io::write_msg_json(stream, msg::Msg::Subscribe, events.as_ref())?.await?;
-    Ok(i3io::read_msg_and::<reply::Success, _>(s).await?)
+    decode_response(stream, decode_event).await
 }
 
 // An easy-to-use subscribe, all you need to do is pass a runtime handle and a `Sender` half of a channel, then listen on
@@ -95,4 +76,3 @@ where
 //     rt.spawn(fut).expect("failed to spawn");
 //     Ok(())
 // }
-
